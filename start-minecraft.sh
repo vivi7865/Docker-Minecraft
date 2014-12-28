be078c5657629a4397ba744a5b3a12ce9b0c8aa6
vivi7865 <vivi7865@users.noreply.github.com>
2014-12-28 19:57:10 +0100

    Create start-minecraft.sh

---
+++ start-minecraft.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+cd /minecraft
+
+if [ ! -e server.jar ]; then
+  echo "Downloading $SOFTWARE ..."
+  wget -o $SERVER_JAR -q $DOWNLOAD_URL
+fi
+
+if [ ! -e server.properties ]; then
+  cp /tmp/server.properties .
+fi
+
+if [ -n "$MOTD" ]; then
+  sed -i "/motd\s*=/ c motd=$MOTD" /minecraft/server.properties
+fi
+
+if [ -n "$WORLD" ]; then
+  sed -i "/level-name\s*=/ c level-name=$WORLD" /minecraft/server.properties
+fi
+
+if [ ! -e /minecraft/eula.txt ]; then
+  if [ "$EULA" != "" ]; then
+    echo "# Generated via Docker on $(date)" > eula.txt
+    echo "eula=$EULA" >> eula.txt
+  else
+    echo ""
+    echo "Please accept the Minecraft EULA at"
+    echo "  https://account.mojang.com/documents/minecraft_eula"
+    echo "by adding the following immediately after 'docker run':"
+    echo "  -e EULA=TRUE"
+    echo ""
+    exit 1
+  fi
+fi
+
+java $JVM_OPTS -jar $SERVER_JAR
